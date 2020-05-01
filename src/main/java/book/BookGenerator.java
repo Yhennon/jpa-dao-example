f29ab0fb42c301c75a095beb97425b1aeac9544b
Yhennon <loostomi@windowslive.com>
2020-05-01 17:51:49 +0200

    Created 'BookGenerator' class, and implemented 'randomBook()','findByIsbn13()' methods.

---
+++ src/main/java/book/BookGenerator.java
@@ -0,0 +1,27 @@
+package book;
+
+import book.model.Book;
+
+import com.github.javafaker.Faker;
+
+import java.time.ZoneId;
+import java.util.Locale;
+
+public class BookGenerator {
+
+    private static Faker faker = new Faker (new Locale("en"));
+
+    private static Book randomBook() {
+        Book book = Book.builder()
+                .isbn13(faker.code().isbn13())
+                .author(faker.book().author())
+                .title(faker.book().title())
+                .format(faker.options().option(Book.Format.class))
+                .publisher(faker.book().publisher())
+                .publicationDate(faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
+                .pages(faker.number().randomDigit())
+                .available(faker.bool().bool())
+                .build();
+        return book;
+    }
+}
