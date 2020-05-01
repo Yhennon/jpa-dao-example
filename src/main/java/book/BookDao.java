6739cf0e8b1658a432c0619974297f88c31badc5
Yhennon <loostomi@windowslive.com>
2020-05-01 17:51:49 +0200

    Created 'BookGenerator' class, and implemented 'randomBook()','findByIsbn13' methods.

--- src/main/java/book/BookDao.java
+++ src/main/java/book/BookDao.java
@@ -1,8 +1,15 @@
 package book;
 
+import book.model.Book;
 import jpa.GenericJpaDao;
+
+import java.util.Optional;
 
 public class BookDao extends GenericJpaDao {
 
+
+    Optional<Book> findByIsbn13(String isbn13) {
+        return Optional.ofNullable(entityManager.find(Book.class, isbn13));
+    }
 
 }
