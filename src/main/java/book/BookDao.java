4d2f50e93379175f4984a5cf7e3ba72c27df1ddd
Yhennon <loostomi@windowslive.com>
2020-05-02 10:41:13 +0200

    Changed findByIsbn13 method

--- src/main/java/book/BookDao.java
+++ src/main/java/book/BookDao.java
@@ -2,13 +2,22 @@ package book;
 
 import book.model.Book;
 
+import com.google.inject.persist.Transactional;
 import jpa.GenericJpaDao;
 
+import javax.persistence.NoResultException;
 import java.util.Optional;
 
 public class BookDao extends GenericJpaDao<Book> {
 
-    Optional<Book> findByIsbn13(String isbn13) {
-        return Optional.ofNullable(entityManager.find(Book.class, isbn13));
+    @Transactional
+    public Optional<Book> findByIsbn13(String isbn13) {
+        try {
+            return Optional.of(entityManager.createQuery("SELECT u FROM Book u WHERE u.isbn13 = :isbn13", Book.class)
+                    .setParameter("isbn13", isbn13)
+                    .getSingleResult());
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
     }
 }
